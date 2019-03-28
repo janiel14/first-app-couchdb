754313155995db9ef99de04a4f7e5c382053b8f0
Janiel Oliveira <janielbelmont@gmail.com>
2019-03-27 23:46:55 -0300

    Create diapers.js

---
+++ app/controllers/diapers.js
@@ -0,0 +1,10 @@
+module.exports = function(app) {
+    const _self = {};
+    const Diapers = app.models.diapers;
+    
+    _self.createOrUpdate = async (req, res) {
+    
+    }
+    
+    return _self;
+}
